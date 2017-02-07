e9d374733c64eabb19ef8e6c91f51a5957caaf1a
Justin Pille <justin.pille@gmail.com>
2017-02-07 09:52:25 -0600

    created two static overlapping lines

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <svg>
+          <line
+            x1="0"
+            y1="200"
+            x2="1000"
+            y2="200"
+            strokeWidth="0.25"
+            stroke="black"
+          />
+          <line
+            x1="0"
+            y1="200"
+            x2="400"
+            y2="200"
+            strokeWidth="1"
+            stroke="black"
+          />
+        </svg>
       </div>
     );
   }
